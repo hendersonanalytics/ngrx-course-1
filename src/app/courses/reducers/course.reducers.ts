9ad69ccb6407011d347dbd360f65d8db2ac7c3d8
hendersonanalytics <everett@hendersonanalytics.com>
2020-08-15 22:07:46 -0500

    end using the ngrx entity adapter

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -1,5 +1,7 @@
 import { Course } from "../model/course";
-import { EntityState } from "@ngrx/entity";
+import { EntityState, createEntityAdapter } from "@ngrx/entity";
+import { createReducer, on } from "@ngrx/store";
+import { CourseActions } from "../course/action-types";
 
 // naive approach: use EntityState instead:
 // https://ngrx.io/guide/entity/interfaces
@@ -14,3 +16,13 @@ export interface CoursesState extends EntityState<Course>{
 
 }
 
+const adapter = createEntityAdapter<Course>();
+
+const initialCoursesState = adapter.getInitialState();
+
+export const coursesReducer = createReducer(
+  initialCoursesState,
+  on(CourseActions.allCoursesLoaded, (state, action) => {
+    return adapter.addAll(action.courses, state);
+  })
+);
