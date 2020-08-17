6aaaf41fa86751216f10afa5065deab9f7561576
hendersonanalytics <everett@hendersonanalytics.com>
2020-08-17 16:08:10 -0500

    end querying store data with ngrx data...

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,34 +1,26 @@
 import {Component, OnInit} from '@angular/core';
-import {compareCourses, Course} from '../model/course';
+import {Course} from '../model/course';
 import {Observable} from "rxjs";
 import {defaultDialogConfig} from '../shared/default-dialog-config';
 import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import {map, shareReplay} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
-
-
+import {map} from 'rxjs/operators';
+import { CourseEntityService } from '../services/course-entity.service';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit { // not sure why this is erroring
 
     promoTotal$: Observable<number>;
-
-    loading$: Observable<boolean>;
-
     beginnerCourses$: Observable<Course[]>;
-
     advancedCourses$: Observable<Course[]>;
-
 
     constructor(
       private dialog: MatDialog,
-      private coursesHttpService: CoursesHttpService) {
-
+      private coursesService: CourseEntityService) {
     }
 
     ngOnInit() {
@@ -36,20 +28,12 @@ export class HomeComponent implements OnInit {
     }
 
   reload() {
-
-    const courses$ = this.coursesHttpService.findAllCourses()
-      .pipe(
-        map(courses => courses.sort(compareCourses)),
-        shareReplay()
-      );
-
-    this.loading$ = courses$.pipe(map(courses => !!courses));
+    const courses$ = this.coursesService.entities$;
 
     this.beginnerCourses$ = courses$
       .pipe(
         map(courses => courses.filter(course => course.category == 'BEGINNER'))
       );
-
 
     this.advancedCourses$ = courses$
       .pipe(
@@ -60,7 +44,6 @@ export class HomeComponent implements OnInit {
         .pipe(
             map(courses => courses.filter(course => course.promo).length)
         );
-
   }
 
   onAddCourse() {
@@ -73,8 +56,5 @@ export class HomeComponent implements OnInit {
     };
 
     this.dialog.open(EditCourseDialogComponent, dialogConfig);
-
   }
-
-
 }
