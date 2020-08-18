a32e6098497cf89877a01ab0fbdca453673c6fc2
hendersonanalytics <everett@hendersonanalytics.com>
2020-08-18 12:48:56 -0500

    end lessons pagination using NgRx data

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -3,11 +3,10 @@ import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
 import {Observable, of} from 'rxjs';
 import {Lesson} from '../model/lesson';
-import {map} from 'rxjs/operators';
+import {map, withLatestFrom, tap, delay} from 'rxjs/operators';
 import {CoursesHttpService} from '../services/courses-http.service';
 import { LessonEntityService } from '../services/lesson-entity.service';
 import { CourseEntityService } from '../services/course-entity.service';
-
 
 @Component({
   selector: 'course',
@@ -19,6 +18,7 @@ export class CourseComponent implements OnInit {
   course$: Observable<Course>;
 
   lessons$: Observable<Lesson[]>;
+  loading$: Observable<boolean>;
 
   displayedColumns = ['seqNo', 'description', 'duration'];
 
@@ -28,7 +28,6 @@ export class CourseComponent implements OnInit {
     private coursesService: CourseEntityService,
     private lessonsService: LessonEntityService,
     private route: ActivatedRoute) {
-
   }
 
   ngOnInit() {
@@ -38,11 +37,30 @@ export class CourseComponent implements OnInit {
       map(courses => courses.find(course => course.url === courseUrl))
     );
 
-    this.lessons$ = of([]);
+    this.lessons$ = this.lessonsService.entities$.pipe(
+      withLatestFrom(this.course$),
+      tap(([lessons, course]) => {
+        if (this.nextPage === 0) {
+          this.loadLessonsPage(course);
+        }
+      }),
+      map(([lessons, course]) => {
+        return lessons.filter(lesson => lesson.courseId === course.id);
+      })
+    );
+
+    // adding delay puts the changes introduced here into the next change detection run
+    this.loading$ = this.lessonsService.loading$.pipe(delay(0));
   }
 
   loadLessonsPage(course: Course) {
+    this.lessonsService.getWithQuery({
+      'courseId': course.id.toString(),
+      'pageNumber': this.nextPage.toString(),
+      'pageSize': '3',
+    });
 
+    this.nextPage += 1;
   }
 
 }
